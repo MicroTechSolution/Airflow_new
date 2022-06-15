7fc9321005ca0e942287c7a4ea790e1ceba4c3fd
Roshan <chothe643@gmail.com>
2022-06-15 19:57:06 +0530

    first commit

---
+++ dags/user_processing.py
@@ -0,0 +1,44 @@
+from asyncio import tasks
+from email.policy import default
+from sched import scheduler
+from airflow.models import DAG
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+
+from datetime import datetime
+
+default_args = {
+    'start_date' : datetime(2022, 5, 19)
+}
+
+
+with DAG('user_processing', schedule_interval='@daily',
+         default_args = default_args,
+         catchup=False) as dag:
+
+    creating_table = SqliteOperator(
+        task_id='creating_table',
+        sqlite_conn_id='db_sqlite',
+        sql='''
+            CREATE TABLE users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL PRIMARY KEY
+
+            );
+        '''
+
+    )
+
+
+    is_api_available = HttpSensor(
+        task_id='is_api_available',
+        http_conn_id='user_api',
+        endpoint='api'
+    )
+
+
+
