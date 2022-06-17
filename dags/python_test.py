08b16e56a5852087e2e0712c3ac1ba9482d44896
Roshan <chothe643@gmail.com>
2022-06-17 11:21:52 +0530

    17jun

--- dags/python_test.py
+++ dags/python_test.py
@@ -6,22 +6,16 @@ import test
 import sys
 import os
 
-
-
 val = Variable.get("Path")
-
-
 
 dag = DAG('hello_world', description='Hello World DAG',
 schedule_interval='0 12 * * *',
-value= val,
 start_date=datetime(2017, 3, 20), catchup=False)
 
 
-
-first_task = Pythonoperator(
+first_task = PythonOperator(
 task_id='first',
-python_callable=value,
+python_callable=val,
 dag=dag)
 
 
